b0b8b025b92e56b46b564ecd85ac993618d73693
Bossa Web Solutions <gvfbossa@hotmail.com>
2025-07-03 20:38:09 -0300

    pequenos ajustes

--- src/main/java/com/centralconsig/propostas/CentralConsigBancosPropostasApplication.java
+++ src/main/java/com/centralconsig/propostas/CentralConsigBancosPropostasApplication.java
@@ -2,8 +2,13 @@ package com.centralconsig.propostas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.centralconsig.propostas",
+		"com.centralconsig.core"
+})
+@EnableScheduling
 public class CentralConsigBancosPropostasApplication {
 
 	public static void main(String[] args) {
